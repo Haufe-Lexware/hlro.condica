b71bd4cff1caadc07c5dc02a6ad5f12df25719d8
mihaido <doru.mihai@haufe-lexware.com>
2017-11-15 00:28:26 +0200

    Added  Functionality for update entry and generate webhooks

--- routes/index.js
+++ routes/index.js
@@ -11,34 +11,84 @@ router.get('/', function(req, res, next) {
 
   models.User.findOrCreate({where: {username: currentUsername}})
   .spread((user, created) => {
-    console.log(user.get({
-      plain: true
-    }));
-    console.log(created);
-    user.getEntries({where: 
-      {
+    console.log(user.get({ plain: true }));
+
+    models.Entry.findOrCreate({ 
+      where:   {
         checkin: {
           [Op.gt]: moment().utc().startOf('day').toDate(),
           [Op.lt]: moment().utc().endOf('day').toDate()
-        }
+        },
+        userid : user.id
       }
-    }).then(entries => {
-      if (entries.length === 0) {
-        user.createEntry({}).then(entry => {
-          return res.render('index', { title: 'Express', identity: req.session.accessToken, entry: entry, user: user });
-        });
-      }
-      else {
-        return res.render('index', { title: 'Express', identity: req.session.accessToken, entry: entries[0], user: user });
-      }
+    }).spread((entry, created) => { 
+      console.log(entry.get({plain:true}));  
+      return res.render('index', { title: 'HLRO Condica', identity: req.session.accessToken, entry: entry, user: user, appUrl: process.env.APP_URL });
     });
   });
-  
 });
 
-router.post('/', function(req,res,next){
+router.post('/', function(req, res, next) {
   console.log(req.body);
-  //TODO: copy paste code from above and update entry in db.
+  models.Entry.findById(req.body.entryId)
+  .then( entry =>  {
+    entry.workedHours = req.body.workedHours;
+    entry.location = req.body.location;
+    entry.overtime = req.body.overtime;
+
+    if (entry.checkOut == null){
+      //TODO: This needs to change if we allow editing dates in the past.
+       entry.checkOut = moment(req.body.checkOut, "HH:mm").toDate();
+    }
+    else {
+      var enteredCheckOutTime = moment(req.body.checkOut, "HH:mm");
+      entry.checkOut = moment(entry.checkOut).hours(enteredCheckOutTime.hours()).minutes(enteredCheckOutTime.minutes());
+    }
+
+    var enteredCheckInTime = moment(req.body.checkIn, "HH:mm");
+    entry.checkIn = moment(entry.checkIn).hours(enteredCheckInTime.hours()).minutes(enteredCheckInTime.minutes());
+    //Object {checkIn: "14:32", checkOut: "", workedHours: "8", overtime: "0", location: "OFFICE", …}
+    //Object {id: "e25b0690-c937-11e7-a008-ef7cd41374cd", checkIn: Tue Nov 14 2017 14:32:31 GMT+0200 (GTB Standard Ti…, checkOut: null, location: null, workedHours: null, …}
+    console.log(entry.get({ plain:true }));  
+    entry.save()
+    .then(() => {
+      console.log(entry.get({ plain:true }));  
+      return res.redirect('/');  
+    });
+  });
 });
+
+router.post('/generatetoken', function(req, res, next){
+    models.User.findById(req.body.userId)
+    .then(user => {
+      user.token = generateRandomString();
+      user.save()
+      .then(() => {
+        console.log(user.get({ plain:true }));  
+        res.redirect('/');
+      });
+    });
+});
+
+// router.get('/:user_id/tasks/:task_id/destroy', function (req, res) {
+  // id: req.params.task_id
+
+router.get('/:token/checkin', function(req, res, next){
+  //TODO: find user by token and set checkin to now()
+});
+
+router.get('/:token/checkout', function(req, res, next){
+  //TODO: find user by token and set checkin to now()
+});
+
+function generateRandomString() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 15; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
 
 module.exports = router;
---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/express-session/index.d.ts" />
+/// <reference path="globals/passport/index.d.ts" />
+/// <reference path="globals/sqlite3/index.d.ts" />
+/// <reference path="modules/sequelize/index.d.ts" />
